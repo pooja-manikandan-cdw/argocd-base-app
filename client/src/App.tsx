f52f90eb91c4017069405a5a7a13e6afccf5e6ef
Pooja Manikandan <Pooja.Manikandan@cdw.com>
2026-04-18 20:12:33 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <Toolbar>
           <PeopleAltIcon sx={{ mr: 1 }} />
           <Typography variant="h6" component="div">
-            User Manager app - argocd assignment app
+            User Manager app
           </Typography>
         </Toolbar>
       </AppBar>
