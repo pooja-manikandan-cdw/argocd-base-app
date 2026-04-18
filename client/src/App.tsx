72243262b72167511bde780740eefed98a9d97c3
Pooja Manikandan <Pooja.Manikandan@cdw.com>
2026-04-18 20:26:18 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <Toolbar>
           <PeopleAltIcon sx={{ mr: 1 }} />
           <Typography variant="h6" component="div">
-            User Manager app
+            User Manager app pooja
           </Typography>
         </Toolbar>
       </AppBar>
