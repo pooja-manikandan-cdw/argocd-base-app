3911f9f41be209b0a6aeec161a47db0dbb94bb36
Pooja Manikandan <Pooja.Manikandan@cdw.com>
2026-04-18 20:06:26 +0530

    Update database.py

--- server/app/database.py
+++ server/app/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./users.db")
 
 engine = create_engine(DATABASE_URL)
 
